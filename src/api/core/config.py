0ac9c98a78d46cc414cd256068f462ed87e0e489
Saeed Farahani <s.farahani@tosantechno.com>
2022-09-03 17:42:40 +0430

    add some configs to check image size

--- src/api/core/config.py
+++ src/api/core/config.py
@@ -5,17 +5,8 @@ from ttutils.TTConfigHandler import confighandler
 sys.path.insert(0, os.pardir)
 # from data.config import current_dir as data_dir
 
+IMAGE_MAX_WIDTH = confighandler.get_config('IMAGE_MAX_WIDTH', 800)
+IMAGE_MIN_WIDTH = confighandler.get_config('IMAGE_MIN_WIDTH', 800)
+IMAGE_MAX_HEIGHT = confighandler.get_config('IMAGE_MAX_HEIGHT', 100)
+IMAGE_MIN_HEIGHT = confighandler.get_config('IMAGE_MIN_HEIGHT', 100)
 
-# SERVICE_NAME=confighandler.get_config('SERVICE_NAME', 'ASSIGNMENT')
-# TARGET_DEPLOY_TYPE=confighandler.get_config('TARGET_DEPLOY_TYPE', 'dev')
-# CUS_DATA = confighandler.get_config("CUS_DATA", "/srv/cus_data")
-# SAVE_INCOMING_DIR = os.path.join(CUS_DATA, 'save', 'in')
-# SAVE_OUTGOING_DIR = os.path.join(CUS_DATA, 'save', 'out')
-# database_dir = os.path.join(CUS_DATA, 'database/')
-# DB_CON_STRING = confighandler.get_config("DB_CON_STRING_ASSIGNMENT", "")
-# log_dir = os.path.join(CUS_DATA, 'logs/')
-
-# BASE_ASSIGNMENT_SERVER_ADDR = confighandler.get_config("BASE_ASSIGNMENT_SERVER_ADDR", "http://assingmentapi:8000") 
-
-# SERVER_LISTEN_IP = confighandler.get_config("SERVER_LISTEN_IP", "0.0.0.0")
-# SERVER_LISTEN_PORT = confighandler.get_config("SERVER_LISTEN_PORT", 8000)
