98c8025a810ae95fed44355e19f67b09329f3855
Saeed Farahani <s.farahani@tosantechno.com>
2022-09-04 12:19:59 +0430

    add a service to read sheet as csv

--- src/contract_api.py
+++ src/contract_api.py
@@ -19,13 +19,29 @@ router = APIRouter()
 #    Contract Api
 #####################################################################################################
 
+@router.post(f"/{SERVICE_NAME}/api/v1/download-contracts-csv-mode", response_model=vm_contract.ResponseBase,
+             tags=["CONTRACTS"])
+async def download_contract_csv(sheet: vm_contract.SheetURL):
+    request_id = get_request_id()
+    response = ContractResponse()
+    try:
+        tt_logger.info(f"download_contract sheet name as csv file")
+        response = await contract_services.download_contracts_csv(sheet.sheet_url)
+        response.log()
+        return response
+
+    except Exception as ex:
+        tt_logger.error(f"Error in download-contracts api request id is {request_id} error is {ex}")
+        return response.set_error()
+
+
 @router.post(f"/{SERVICE_NAME}/api/v1/download-contracts", response_model=vm_contract.ResponseBase, tags=["CONTRACTS"])
-async def download_contract(sheet: vm_contract.Sheet):
+async def download_contract(sheet: vm_contract.SheetName):
     request_id = get_request_id()
     response = ContractResponse()
     try:
         tt_logger.info(f"download_contract sheet name {sheet.sheet_name}")
-        response = await contract_services.download_contracts(sheet.sheet_url, sheet.sheet_name)
+        response = await contract_services.download_contracts_csv(sheet.sheet_url, sheet.sheet_name)
         response.log()
         return response
 
